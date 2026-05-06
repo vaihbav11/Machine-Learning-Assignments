fd330076e8c7cacccb7ba348e8600de0ae2f75cf
Vaibhav Chaturvedi <110451139+vaihbav11@users.noreply.github.com>
2026-05-06 23:55:26 +0530

    Update and rename delivery_cnn.py to ass4.py

--- ass4.py
+++ ass4.py
@@ -4,7 +4,7 @@ subprocess.run(["pip", "install", "kagglehub", "tensorflow", "scikit-learn",
 
 import kagglehub
 import os
-import numpy as np
+import numpy as np 
 import pandas as pd
 from sklearn.model_selection import train_test_split, StratifiedKFold
 from sklearn.preprocessing import StandardScaler, LabelEncoder
